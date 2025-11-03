981817fcacbf4edcd89f921a8918a595e80541f4
veeoid <vc7410@rit.edu>
2025-11-02 22:18:33 -0600

    Initial Structure

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 8001;
+
+app.listen(PORT, ()=>{console.log(`listening at PORT: ${PORT}, use http://localhost:${PORT}`)})
