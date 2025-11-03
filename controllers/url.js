304703319a300e870baac1d34c0ed8f7a68a7225
veeoid <vc7410@rit.edu>
2025-11-02 23:22:03 -0600

    added visitHistory Functionality

--- controllers/url.js
+++ controllers/url.js
@@ -17,6 +17,18 @@ async function handleGenerateNewShortURL(req, res) {
     return res.json({id: shortId});
 }
 
+
+async function handleGetAnalytics(req, res) {
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({shortId})
+    return res.json(
+        {
+            totalClicks:result.visitHistory.length, 
+            analytics:result.visitHistory
+        })
+};
+
 module.exports = {
     handleGenerateNewShortURL,
+    handleGetAnalytics
 }
